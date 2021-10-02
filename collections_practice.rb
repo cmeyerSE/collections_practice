a316d94daeef84a1bd4ef622a7d3faf421724593
Cory <cameyer182@gmail.com>
2021-10-01 22:43:38 -0400

    commit tree 669b7f4f46a1586f2b92c1a3a067b522544cb539

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,99 @@
+def sort_array_asc (array)
+    array.sort
+  end
+  
+  
+  
+  def sort_array_desc (array)
+    array.sort {|x,y| y <=> x }
+  end
 
+
+  def sort_array_char_count(array)
+    array.sort_by do |x|
+       x.length
+     end
+  end
+  
+  
+  
+  def swap_elements (array)
+    array[1], array[2] = array[2], array[1]
+    array
+  end
+  
+  
+  
+  def reverse_array (array)
+    array.reverse
+  end
+  
+  
+  
+  def kesha_maker (array)
+    array.collect do |element|
+      element[2] = "$"
+      element
+    end
+  end
+ 
+  
+  
+  def find_a (array)
+    array.select do |word|
+      word.chr == "a"
+    end
+  end
+  
+  
+  
+  def sum_array (array)
+    array.inject do |sum, num|
+      sum + num
+    end
+  end
+  
+  
+  
+  def add_s (array)
+    array.collect.with_index do |word, index|
+      if index == 1 
+        word 
+      else 
+        word + "s"
+      end
+    end
+  end
+  
+  
+  
+  def count_words (words)
+    story_count = Hash.new
+  
+    words.split.each do |word|
+      if story_count.include?(word)
+        story_count[word] = story_count[word] + 1
+      else
+        story_count[word] = 1
+      end
+    end
+  
+    story_count
+  end
+  
+  
+  
+  def organize_songs_by_artist (tracks)
+    hash = {}
+    tracks.each do |name|
+      artist = name.split(" - ")[0]
+      trackname = name.split(" - ")[1]
+  
+      if hash.include?(artist)
+        hash[artist] << trackname
+      else
+        hash[artist] = [] << trackname
+      end
+    end
+    hash
+  end 
